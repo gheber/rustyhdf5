00b58d02807985649adede6fa645cb09863b2d5e
Gerd Heber <gheber@hdfgroup.org>
2026-03-05 23:53:23 +0000

    Refactor Python packaging tool installation to use virtual environment

--- .devcontainer/scripts/post-create.sh
+++ .devcontainer/scripts/post-create.sh
@@ -21,15 +21,16 @@ for target in "${RUST_TARGETS[@]}"; do
 done
 
 echo "==> Installing Python packaging tools"
-python3 -m pip install --user --upgrade pip
-python3 -m pip install --user maturin
+python3 -m venv venv
+./venv/bin/python3 -m pip install --user --upgrade pip
+./venv/bin/python3 -m pip install --user maturin
 
 echo "==> Toolchain versions"
 rustc --version
 cargo --version
 cargo fmt --version
 cargo clippy --version
-python3 --version
+./venv/bin/python3 --version
 
 echo "==> Verifying workspace metadata"
 cargo metadata --no-deps >/dev/null
